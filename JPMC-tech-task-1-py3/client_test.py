ae36ae36dceeac71707860e17ef72bc0765225de
Gokul9099 <r.gokulrk9@gmail.com>
2020-06-04 23:11:20 -0400

    Task-1

---
+++ JPMC-tech-task-1-py3/client_test.py
@@ -0,0 +1,37 @@
+import unittest
+from client3 import getDataPoint
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for i in quotes:
+      stock = i['stock']
+      bid_price = float(i['top_bid']['price'])
+      ask_price = float(i['top_ask']['price'])
+      price = (bid_price + ask_price) / 2
+      self.assertEqual(getDataPoint(i), (stock, bid_price, ask_price, price))
+    
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for i in quotes:
+      stock = i['stock']
+      bid_price = float(i['top_bid']['price'])
+      ask_price = float(i['top_ask']['price'])
+      price = (bid_price + ask_price) / 2
+      self.assertEqual(getDataPoint(i), (stock, bid_price, ask_price, price))
+
+  """ ------------ Add more unit tests ------------ """
+
+
+
+if __name__ == '__main__':
+    unittest.main()
